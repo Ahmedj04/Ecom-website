01b5479cad4cc7b31accf40f710a1251565b4db9
Ahmed <janahmed902@gmail.com>
2023-08-15 14:56:05 +0530

    created header and made it responsive

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { GoSearch } from "react-icons/go";
 
-function Header() {
-  return (
-    <header>
-        
-    </header>
-  )
+
+function Header({setMenu}) {
+    return (
+        <header>
+            <div className='px-4 mx-4 flex justify-between'>
+
+                {/* menu options for lg screen */}
+                <div className='hidden lg:block my-auto lg:px-10 lg:w-5/12'>
+                    <ul className='flex text-lg'>
+                        <li><a href='#home' className='pr-5 cursor-pointer hover:underline'>Home</a></li>
+                        <li><a href='#rooms' className='pr-5 cursor-pointer hover:underline'>Rooms</a></li>
+                        <li><a href='#photos' className='pr-5 cursor-pointer hover:underline'>Gallery</a></li>
+                        <li><a href='#services' className='pr-5 cursor-pointer hover:underline'>Services</a></li>
+                    </ul>
+                </div>
+
+
+                {/* logo */}
+                <div className='px-3 lg:w-5/12'>
+                    <div className='py-5'>
+                        <a><img className='w-24 md:w-32 lg:w-36' src='https://learts-demo.myshopify.com/cdn/shop/files/logo-2_225x.png?v=1613704039'></img></a>
+                    </div>
+
+                </div>
+
+                {/* options */}
+                <div className='px-0 my-auto flex justify-between lg:w-2/12 lg:justify-end'>
+                    <i className='mr-5'><MdOutlineShoppingCart size={25} /></i>
+                    {/* menu option for sm & md screen */}
+                    <i className='lg:hidden' onClick={() => setMenu(1)}><RxHamburgerMenu size={25} /></i>
+                    <i className='hidden lg:block'><GoSearch size={25} /></i>
+                </div>
+
+            </div>
+
+        </header>
+    )
 }
 
 export default Header
--- pages/ecom.js
+++ pages/ecom.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from '@/components/Header'
+import { AiOutlineClose } from "react-icons/ai";
+
 
 function Ecom() {
-  return (
-    <main>
-        
-    </main>
-  )
+
+    const [menu, setMenu] = useState(0)
+
+    return (
+        <main>
+            <Header 
+                setMenu={setMenu}
+            />
+
+            {/* menu for small screen */}
+            <div className={`transition-transform duration-500 ease-in-out transform ${menu === 1 ? 'translate-x-0' : '-translate-x-full'} fixed inset-0 z-50`}>
+                {/* Dark background */}
+                <div className={`absolute inset-0 bg-black opacity-70 transform transition-transform duration-500 ease-in-out ${menu === 1 ? 'translate-x-0' : '-translate-x-full'}`}></div>
+
+                {/* Content */}
+                <div className={`absolute inset-y-0 left-0 w-8/12 bg-custom-brown p-4 transform transition-transform duration-1000 ease-in-out ${menu === 1 ? 'translate-x-0 delay-300' : '-translate-x-full'}`}>
+                    <div className='flex justify-between'>
+                        <div className=' inline-block'>
+                            <a><img className='w-28' src='https://learts-demo.myshopify.com/cdn/shop/files/logo-2_225x.png?v=1613704039'></img></a>
+                        </div>
+                        <button onClick={() => setMenu(0)}><AiOutlineClose /></button>
+                    </div>
+
+                    <div>
+                        <ul className='pt-10 text-lg' onClick={() => setMenu(0)}>
+                            <li className='pb-2'><a href='#about'>About</a></li>
+                            <li className='pb-2'><a href='#rooms'>Rooms</a></li>
+                            <li className='pb-2'><a href='#photos'>Gallery</a></li>
+                            <li><a href='#services'>Services</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    )
 }
 
 export default Ecom
